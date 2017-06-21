e898dfb5caf475663a49abd31742418ee75381c1
a-tsu <lab_is_truth@yahoo.co.jp>
2017-06-21 23:56:07 +0900

    plusplus es6

---
+++ plusplus-es6.js
@@ -0,0 +1,18 @@
+let i = 10;
+const j = 20;
+
+i ++;
+j
+console.log(i);
+console.log(j);
+// i = 11, j = 20
+
+const k = 10;
+let m = 20;
+
+k
+;++
+m
+console.log(k);
+console.log(m);
+// k = 10, m = 21
